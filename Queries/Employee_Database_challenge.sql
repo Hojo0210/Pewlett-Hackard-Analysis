74a3f1d84c0b40458f99cedfa00e9251a547f2f3
Chris E <ceife0210@gmail.com>
2021-06-13 12:36:10 -0400

    added challenge file

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,32 @@
+
+--- Joining title and employee tables
+SELECT e.emp_no,
+     e.first_name,
+     e.last_name,
+	 t.title,
+	 t.from_date,
+     t.to_date
+into retirement_titles
+from employees as e
+join titles as t
+on e.emp_no = t.emp_no
+where (e.birth_date between '1952-01-01' and '1955-12-31')
+order by emp_no;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Retrieve number of employees (by title) that are about to retire
+select count(title) , title
+into retiring_titles
+from unique_titles
+group by title
+order by count(title) desc;
