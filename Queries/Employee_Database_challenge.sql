8f5bc22d8cbe6f6350850dc3f4103bec5780d231
Chris E <ceife0210@gmail.com>
2021-06-13 21:11:59 -0400

    second pass on challenge file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,21 @@ into retiring_titles
 from unique_titles
 group by title
 order by count(title) desc;
+
+-- query for mentorship eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date, 
+	de.from_date, 
+	de.to_date, 
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
+JOIN titles as t
+	ON e.emp_no = t.emp_no
+where (t.to_date = '9999-01-01')
+and (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
