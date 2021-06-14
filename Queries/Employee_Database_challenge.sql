2c2913e861ac8809366588d0318ebc4c269abf63
Chris E <ceife0210@gmail.com>
2021-06-13 21:17:31 -0400

    second pass on challenge file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -6,12 +6,12 @@ SELECT e.emp_no,
 	 t.title,
 	 t.from_date,
      t.to_date
-into retirement_titles
-from employees as e
-join titles as t
-on e.emp_no = t.emp_no
-where (e.birth_date between '1952-01-01' and '1955-12-31')
-order by emp_no;
+INTO retirement_titles
+FROM employees as e
+JOIN titles as t
+ON e.emp_no = t.emp_no
+where (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
 
 
 -- Use Dictinct with Orderby to remove duplicate rows
@@ -25,11 +25,11 @@ FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
 -- Retrieve number of employees (by title) that are about to retire
-select count(title) , title
-into retiring_titles
-from unique_titles
-group by title
-order by count(title) desc;
+SELECT count(title) , title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count(title) desc;
 
 -- query for mentorship eligibility
 SELECT DISTINCT ON (e.emp_no) e.emp_no, 
